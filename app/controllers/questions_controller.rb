080e2ae817cb95164c4bac9529a7abdb1b339c01
Philipp Preuss <philipp.arian@gmail.com>
2018-05-10 14:31:06 +0800

    first assignement

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,17 +1,16 @@
 class QuestionsController < ApplicationController
-
   def ask
 
   end
 
   def answer
-    if params[:user_question] =="I am going to work"
+    @question = params[:question]
+    if @question == "I am going to work"
       @answer = "Great!"
-    elsif params[:user_question].end_with? "?"
-      @answer =  "Silly question, get dressed and go to work!"
+    elsif @question.last == "?"
+      @answer = "Silly question, get dressed and go to work!"
     else
-      @answer =  "I don't care, get dressed and go to work!"
+      @answer = "I don't care, get dressed and go to work!"
     end
-
   end
 end
